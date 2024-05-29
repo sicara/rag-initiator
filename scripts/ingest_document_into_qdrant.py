ed2ab22fc56d5b6368306af39234f018c30851f9
grégoire <sicara10@MacBook-Pro-de-Sicara10.local>
2024-05-29 17:37:41 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:scripts/ingest_document_into_qdrant.py

--- scripts/ingest_document_into_qdrant.py
+++ scripts/ingest_document_into_qdrant.py
@@ -1,21 +1,30 @@
+#%%
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Qdrant
 
-from src.constants import INPUT_DATA_FOLDER
+from src.constants import INPUT_DATA_FOLDER, DATA_FOLDER_PATH
 from src.document_ingestion.split_documents import split_pdf_into_chunks
 
 embeddings = OpenAIEmbeddings()
 
 
 document_paths = (INPUT_DATA_FOLDER / "documents").glob("*.pdf")
+vector_store_path = DATA_FOLDER_PATH / "qdrant"
+vector_store_path.mkdir(exist_ok=True, parents=True)
 texts = []
 for document_path in document_paths:
     texts.extend(split_pdf_into_chunks(document_path))
 
+
+
+
 doc_store = Qdrant.from_documents(
     texts,
     embeddings,
-    url="<qdrant-url>",
-    api_key="<qdrant-api-key>",
+    path = vector_store_path,
     collection_name="texts",
 )
+
+
+
+# %%
