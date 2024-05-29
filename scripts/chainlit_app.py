28ad23ec96f4345ab683bdd160416593dda60ff0
Hugo Khlaut <hugo.khlaut@sicara.com>
2024-05-29 18:11:31 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:chainlit.md

--- scripts/chainlit_app.py
+++ scripts/chainlit_app.py
@@ -1,15 +1,44 @@
 # %%
-from langchain.vectorstores import Qdrant
-from langchain_openai import OpenAIEmbeddings
-from qdrant_client import QdrantClient
+import chainlit as cl
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable.config import RunnableConfig
 
-from src.constants import DATA_FOLDER_PATH
+# vector_store_path = DATA_FOLDER_PATH / "qdrant"
+# embeddings = OpenAIEmbeddings()
+# client = QdrantClient(path=str(vector_store_path))
+# db = Qdrant(client=client, collection_name="texts", embeddings=embeddings)
 
-vector_store_path = DATA_FOLDER_PATH / "qdrant"
-embeddings = OpenAIEmbeddings()
-client = QdrantClient(path=str(vector_store_path))
-db = Qdrant(client=client, collection_name="texts", embeddings=embeddings)
+# db.similarity_search(query="hello world", k=5)
 
-db.similarity_search(query="hello world", k=5)
 
-# %%
+@cl.on_chat_start
+async def on_chat_start() -> None:
+    model = ChatOpenAI(streaming=True)
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You're a very knowledgeable historian who provides accurate and eloquent answers to historical questions.",
+            ),
+            ("human", "{question}"),
+        ]
+    )
+    runnable = prompt | model | StrOutputParser()
+    cl.user_session.set("runnable", runnable)
+
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    runnable = cl.user_session.get("runnable")  # type: Runnable
+
+    msg = cl.Message(content="")
+
+    async for chunk in runnable.astream(
+        {"question": message.content},
+        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+    ):
+        await msg.stream_token(chunk)
+
+    await msg.send()
